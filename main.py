25d715f174691c6ea5112acb724c3250731d01d4
carlos fonseca <sdluizcarlos.psn@gmail.com>
2024-02-06 22:23:29 -0300

    fix: pt-br error config in setxkbmap

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ class KeywordQueryEventListener(EventListener):
         elif query == extension.preferences['nl']:
             subprocess.Popen(['setxkbmap', 'nl'])
         elif query == extension.preferences['kbr']:
-            subprocess.Popen(['setxkbmap', '-model', 'abnt2', '-layout', 'br'])
+            subprocess.Popen(['setxkbmap', 'br', '-variant', 'abnt2'])
         return HideWindowAction()
 
 
